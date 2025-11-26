fa0e6d515183d86212fdaa1e9a2671faf98aeb5a
Zhiqiang Wang <zhiqiang@zhiqiangs-mbp.mynetworksettings.com>
2025-11-25 23:22:15 -0500

    update index.html scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,63 @@
+// Data
+
+const playlist = [
+    {
+        'videoId': '', 
+        'streetviewIds': [
+
+        ],
+    },
+    {
+        'videoId': '',
+        'streetviewIds': [
+
+        ],
+    },
+    {
+        'videoId': '',
+        'streetviewIds': [
+
+        ],
+    },
+];
+
+//
+const previousVideoBtn = document.getElementById('previous-video-btn');
+const nextVideoBtn = document.getElementById('next-video-btn');
+let currentVideoIdx = 0;
+let player;
+
+if (currentVideoIdx === 0) {
+    previousVideoBtn.disabled = true;
+}
+if (playlist.length === 1) {
+    nextVideoBtn.disabled = true;
+}
+
 // Load IFrame Player API asynchronously
 const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 document.head.appendChild(tag);
 
-let player;
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '150',
         width: '150',
-        videoId: 'ZIJtvsf8uCc', // sample video
+        videoId: playlist[currentVideoIdx]['videoId'],
         events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange,
         }
     });
 }
 
-function onPlayerReady(event) {
-    // autoplay or control manually
-    // event.target.playVideo();
-}
+previousVideoBtn.addEventListener('click', function(event) {
+    currentVideoIdx += 1;
+});
 
-function onPlayerStateChange(event) {
-    //
-}
+nextVideoBtn.addEventListener('click', function(event) {
+    currentVideoIdx -= 1;
+});
+
+streetviewMask = document.getElementById('streetview-mask');
+streetviewMask.classList.remove('opacity-0');
+streetviewMask.classList.add('opacity-75');
